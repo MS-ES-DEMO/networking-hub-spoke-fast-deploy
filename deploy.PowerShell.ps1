d9b457c2d7b0a277df6b4376cb2dbb4ce4550fc7
Bosco Domingo <boscodomingo@microsoft.com>
2022-01-07 13:15:04 +0100

    Improve Powershell deploy

--- deploy.PowerShell.ps1
+++ deploy.PowerShell.ps1
@@ -1,20 +1,20 @@
 param (
-  [string]
-  $location = "westeurope",
-  [string] 
-  $templateFile = ".\main.bicep",
-  [string]
-  $parameterFile = "parameters.json",
-  [string] 
-  $deploymentPrefix='Network-Fundamentals'
-  )
+    [string]
+    $location = "westeurope",
+    [string]
+    $templateFile = ".\main.bicep",
+    [string]
+    $parameterFile = "parameters.json",
+    [string]
+    $deploymentPrefix = 'Network-Fundamentals'
+)
 
-$deploymentName = $deploymentPrefix
+$date = $((Get-Date -UFormat "%Y-%m-%dT%H-%M-%S_%ZZ").Replace("+", ""))
+$deploymentName = "$deploymentPrefix-$($date)"
 
 
 New-AzDeployment -Name $deploymentName `
-                -Location $location `
-                -TemplateFile $templateFile `
-                -TemplateParameterFile $parameterFile `
-                -Verbose
-
+    -Location $location `
+    -TemplateFile $templateFile `
+    -TemplateParameterFile $parameterFile `
+    -Verbose
